1c22b83ee97d371ec75dac25fe003aaccd7fdfab
Meatclump <hepe22@hotmail.com>
2023-03-09 17:44:16 -0600

    add app config requirements for session handling

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import json
 from blueprints.views import views_bp
 import modules.db_interface as db_interface
+settings = json.loads(open('config.json').read())
 
 app = Flask(__name__)
+app.config.update(settings)
 
 app.register_blueprint(views_bp)
 
